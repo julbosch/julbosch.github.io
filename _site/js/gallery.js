263680fe3ed5acc29b137be13ed74699c5311ee8
Julius Bosch <juliusbosch89@gmail.com>
2018-09-16 02:36:16 -0700

    Finish javascript gallery

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -1,3 +1,30 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
-}
+
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo > img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let newImageAlt = thumbnail.dataset.title;
+
+      // swap out the main image for the selected thumbnail
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", newImageAlt);
+
+      // Change selected thumbnail to "current" to style it correctly
+      let currentClass = "current"
+      
+      document.querySelector(`.${currentClass}`).classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Change the decription column to have the title and description of selected image
+      let title       = document.querySelector("#gallery-info > .title");
+      let description = document.querySelector("#gallery-info > .description");
+
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
+
+};
